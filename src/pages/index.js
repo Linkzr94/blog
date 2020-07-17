7be3a9dcd7bd456105584761b01ea6d805524b65
linkzr <1028159057@qq.com>
2020-07-17 16:04:56 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
+      <p>deploy test</p>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
